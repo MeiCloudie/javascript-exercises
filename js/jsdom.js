01eb77feaf51903805d69fa8930911a9814cd103
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-06-05 17:13:36 +0700

    feat(jsdom): Complete BT2

--- js/jsdom.js
+++ js/jsdom.js
@@ -14,6 +14,26 @@ document.getElementById("formTinhTienLuongNV").onsubmit = function (event) {
   document.getElementById("xuatLuong").innerText =
     tongLuong.toLocaleString("vi", {
       currency: "VND",
-    //   style: "currency",
+      //   style: "currency",
     }) + " VND"
+}
+
+//BÀI TẬP 2
+document.getElementById("formTinhGiaTriTrungBinh").onsubmit = function (event) {
+  // Ngăn chặn hành vi mặc định của form khi submit
+  event.preventDefault()
+
+  // Lấy giá trị từ các input
+  var soThuNhat = parseFloat(document.getElementById("soThuNhat").value)
+  var soThuHai = parseFloat(document.getElementById("soThuHai").value)
+  var soThuBa = parseFloat(document.getElementById("soThuBa").value)
+  var soThuTu = parseFloat(document.getElementById("soThuTu").value)
+  var soThuNam = parseFloat(document.getElementById("soThuNam").value)
+
+  // Tính toán giá trị trung bình
+  var tong = soThuNhat + soThuHai + soThuBa + soThuTu + soThuNam
+  var giaTriTrungBinh = tong / 5
+
+  // Hiển thị kết quả
+  document.getElementById("xuatTrungBinh").innerText = giaTriTrungBinh
 }
